04e72272151d6c9515822ade8f615917afaefbf7
juguarnieri <juliaguarnieri04@gmail.com>
2025-03-14 15:20:54 -0300

    feat: create color for drawer

--- navigation/DrawerNavigation.js
+++ navigation/DrawerNavigation.js
@@ -8,7 +8,18 @@ const Drawer = createDrawerNavigator();
 
 const DrawerNavigator = () => {
   return (
-    <Drawer.Navigator>
+    <Drawer.Navigator
+      screenOptions={{
+        drawerStyle: {
+          backgroundColor: '#6F2739',
+        },
+        drawerActiveTintColor: '#fff', 
+        drawerInactiveTintColor: '#bbb', 
+        drawerLabelStyle: {
+          fontWeight: 'bold', 
+        },
+      }}
+    >
       <Drawer.Screen name="Home" component={HomeScreen} />
       <Drawer.Screen name="Detalhes" component={Detalhes} />
       <Drawer.Screen name= "Perfil" component= {Perfil} />
